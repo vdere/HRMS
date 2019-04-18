94689e2ed6ca8ad258d9113d4d6dd7000b0b8c98
Dere <mjviraj123@gmail.com>
2019-04-18 22:08:50 +0530

    Exception Class

---
+++ HRMS.Exception/HRMSException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Exceptions
+{
+    public class HRMSException : ApplicationException
+    {
+        //Default constructor
+        public HRMSException()
+            : base()
+        { }
+
+        //Parameterized constructor with message parameter
+        public HRMSException(string Message) : base(Message)
+        { }
+    }
+}
